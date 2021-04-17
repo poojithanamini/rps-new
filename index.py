ff2283b41dbcc8f8ffb468ee88510eb4c44b1b6e
poojithanamini <60817077+poojithanamini@users.noreply.github.com>
2021-04-17 16:26:47 +0530

    making better

--- index.py
+++ index.py
@@ -1,26 +1,78 @@
 import streamlit as st
 import random
 
-st.write("Choose your weapon Rock, Paper, or Scissors: ")
+option = st.sidebar.selectbox('Select', ['Rock, Paper, Scissor', 'With Lizard and Spock'])
+if option == 'Rock, Paper, Scissor':
+    st.title('ROCK, PAPER, SCISSORS')
 
-userChoice = st.radio('', ['Rock', 'Paper', 'Scissors'])
+    st.write("Choose your weapon - Rock, Paper, or Scissors: ")
 
-choices = ['Rock', 'Paper', 'Scissors']
-opponenetChoice = random.choice(choices)
+    userChoice = st.radio('', ['Rock', 'Paper', 'Scissors'])
 
-st.write("You chose: "+userChoice)
-st.write("I chose: " + opponenetChoice)
+    choices = ['Rock', 'Paper', 'Scissors']
+    opponentChoice = random.choice(choices)
 
-if opponenetChoice == userChoice:   
-    st.write("Tie! 😁")
-elif opponenetChoice == 'Rock' and userChoice == 'Scissors':      
-    st.write("Scissors beats rock, I win! 🎉")
-    pass
-elif opponenetChoice == 'Scissors' and userChoice == 'Paper':          
-    st.write("Scissors beats paper, I win! 🎉")
-    pass
-elif opponenetChoice == 'Paper' and userChoice == 'Rock':      
-    st.write("Paper beats rock, I win! 🎉")
-    pass
-else:       
-    st.write("You win! 😒")
+    st.write("You chose: "+userChoice)
+    st.write("I chose: " + opponentChoice)
+
+    if opponentChoice == userChoice:   
+        st.write("Tie! 😁")
+    elif opponentChoice == 'Rock' and userChoice == 'Scissors':      
+        st.write("Rock crushes Scissors, I win! 🎉")
+        pass
+    elif opponentChoice == 'Scissors' and userChoice == 'Paper':          
+        st.write("Scissors cuts Paper, I win! 🎉")
+        pass
+    elif opponentChoice == 'Paper' and userChoice == 'Rock':      
+        st.write("Paper covers Rock, I win! 🎉")
+        pass
+    else:       
+        st.write("You win! 😒")
+if option == 'With Lizard and Spock':
+    st.title('ROCK, PAPER, SCISSORS, LIZARD, SPOCK')
+    st.write('')
+    st.write('')
+    st.write("Choose your weapon - Rock, Paper, Scissors, Lizard, Spock: ")
+
+    userChoice = st.radio('', ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'])
+
+    choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+    opponentChoice = random.choice(choices)
+
+    st.write("You chose: "+userChoice)
+    st.write("I chose: " + opponentChoice)
+
+    if opponentChoice == userChoice:   
+        st.write("Tie! 😁")
+    elif opponentChoice == 'Scissors' and userChoice == 'Paper':      
+        st.write("Scissors cuts Paper, I win! 🎉")
+        pass
+    elif opponentChoice == 'Paper' and userChoice == 'Rock':          
+        st.write("Paper covers Rock, I win! 🎉")
+        pass
+    elif opponentChoice == 'Rock' and userChoice == 'Lizard':      
+        st.write("Rock crushes Lizard, I win! 🎉")
+        pass
+    elif opponentChoice == 'Spock' and userChoice == 'Scissors':      
+        st.write("Spock smashes Scissors, I win! 🎉")
+        pass
+    elif opponentChoice == 'Lizard' and userChoice == 'Spock':      
+        st.write("Lizard poisons Spock, I win! 🎉")
+        pass
+    elif opponentChoice == 'Scissors' and userChoice == 'Lizard':      
+        st.write("Scissors decapitates Lizard, I win! 🎉")
+        pass
+    elif opponentChoice == 'Lizard' and userChoice == 'Paper':      
+        st.write("Lizard eats Paper, I win! 🎉")
+        pass
+    elif opponentChoice == 'Paper' and userChoice == 'Spock':      
+        st.write("Paper disproves Spock, I win! 🎉")
+        pass
+    elif opponentChoice == 'Spock' and userChoice == 'Rock':      
+        st.write("Spock vaporizes Rock, I win! 🎉")
+        pass
+    elif opponentChoice == 'Rock' and userChoice == 'Scissors':      
+        st.write("Rock crushes Scissors, I win! 🎉")
+        pass
+    else:       
+        st.write("You win! 😒")
